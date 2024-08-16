b73a409162a88242da66bd5a494006a409d56c14
VeronicaDomR <99091553+VeronicaDomR@users.noreply.github.com>
2024-08-16 16:09:15 -0600

    sql

--- sql/clean_data.sql
+++ sql/clean_data.sql
@@ -5,9 +5,11 @@ SELECT
     start_station_id, start_station_name, 
     start_station_latitude, start_station_longitude, end_station_id, 
     end_station_name, end_station_latitude, end_station_longitude, bikeid, 
-    usertype, birth_year, gender
+    usertype, birth_year, gender,
+    EXTRACT(YEAR FROM CURRENT_DATE()) - birth_year AS age
 FROM 
     `citybikes-432718.citybikestrips.city_bike_trips`
 WHERE 
     birth_year IS NOT NULL
     AND gender != 'unknown';
+
